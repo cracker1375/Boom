28a8482ba3871e1fec34bef8195f13b423661917
cracker1375 <cracker1375@gmail.com>
2017-01-01 18:14:19 +0330

    Update short_link.lua

--- plugins/short_link.lua
+++ plugins/short_link.lua
@@ -1,15 +1,4 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Hessam 
-# ID : @H_Terminal
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
+
 local function DevPoint(msg, matches)
   local reza = URL.escape(matches[1])
   url = "https://api-ssl.bitly.com/v3/shorten?access_token=f2d0b4eabb524aaaf22fbc51ca620ae0fa16753d&longUrl="..reza

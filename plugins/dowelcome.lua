dd8e329c4f83c85297aea46a29b439504be12ab9
cracker1375 <cracker1375@gmail.com>
2017-01-01 17:22:36 +0330

    Update dowelcome.lua

--- plugins/dowelcome.lua
+++ plugins/dowelcome.lua
@@ -1,15 +1,4 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Hessam
-# ID : @H_Terminal
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
+
 local function yagop(msg, matches, callback, extra)
 
 local data = load_data(_config.moderation.data)

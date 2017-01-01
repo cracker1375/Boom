77e5d0870d0e07c98b161b3bdae13ac6253d32fb
cracker1375 <cracker1375@gmail.com>
2017-01-01 12:46:19 +0330

    Update dowelcome.lua

--- plugins/dowelcome.lua
+++ plugins/dowelcome.lua
@@ -3,8 +3,8 @@
 #ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 #:((
 # For More Information ....! 
-# Developer : reza < @Yagop > 
-# our channel: @Ntflight
+# Developer : Hessam
+# ID : @H_Terminal
 # Version: 1.1
 #:))
 #ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
@@ -19,7 +19,7 @@ local group_welcome = data[tostring(msg.to.id)]['group_welcome']
 if matches[1] == 'chat_add_user' or 'chat_add_user_link' then --For Normal Group
     if not msg.service then 
         return nil 
-    else--@Ntflight
+    else--@H_Terminal
 if string.match(group_welcome, '{gpname}') then group_welcome = string.gsub(group_welcome, '{gpname}', ""..string.gsub(msg.to.print_name, "_", " ").."")
  end
 if string.match(group_welcome, '{firstname}') then group_welcome = string.gsub(group_welcome, '{firstname}', ""..(msg.action.user.first_name or '').."")

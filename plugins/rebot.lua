1cf6e4343ee0bb65be5e389fa9e69450284058c1
cracker1375 <cracker1375@gmail.com>
2017-01-01 13:26:03 +0330

    Update rebot.lua

--- plugins/rebot.lua
+++ plugins/rebot.lua
@@ -3,7 +3,7 @@ do
 local function run(msg, matches) 
 if ( msg.text ) then
   if ( msg.to.type == "user" ) then
-     return "- Welcome to BombusMOD !\n\n- For More Information Subscribe in \n\n- Channel : @Ntflight"
+     return "- Welcome to BOOM BOOM !\n\n- For More Information Subscribe in \n\n- ID : @H_Terminal"
     end 
   end 
 end 
